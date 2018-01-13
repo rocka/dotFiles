a0bb432ee37f097163422e8abf209a9c2642f118
rocka <i@rocka.me>
2018-01-13 17:39:20 +0800

    import config again

--- .config/fish/conf.d/rocka.fish
+++ .config/fish/conf.d/rocka.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/fish
 
-alias l="ls -Alh"
-alias vi="vim"
-alias qwq="uname -nmsr;and uptime"
+alias l="ls -lh"
+alias ll="ls -Alh"
+alias pq="proxychains -q"
+alias qwq="uname -snrm; uptime"
 alias clip="xclip -selection clipboard"
+alias dokku="env DOKKU_HOST=dokku.rocka.me dokku"
