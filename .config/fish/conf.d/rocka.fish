ee37f8869c2f50ac1044e800ee61746f6ace66cf
Rocka <i@rocka.me>
2019-03-11 16:47:04 +0800

    fish: alias ls with `--time-style=long-iso`

--- .config/fish/conf.d/rocka.fish
+++ .config/fish/conf.d/rocka.fish
@@ -1,9 +1,18 @@
 #!/usr/bin/fish
 
+alias ls="ls --color=auto --indicator-style=classify --time-style=long-iso"
 alias l="ls"
 alias ll="ls -lh"
-alias la="ls -Alh"
+alias la="ls -alh"
 alias pq="proxychains -q"
 alias qwq="uname -snrm; uptime"
 alias cman="env LANG=zh_CN.UTF-8 man"
 alias clip="xclip -selection clipboard"
+
+alias ...="popd"
+
+function mkws
+    set DIR (date +%Y-%m-%d_$argv[1])
+    mkdir -p ~/Workspace/$DIR
+    pushd ~/Workspace/$DIR
+end
