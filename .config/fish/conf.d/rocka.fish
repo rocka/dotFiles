f5a39a00bfa3ab4281294e890dbe371dab0d3228
Rocka <i@rocka.me>
2025-05-18 01:53:03 +0800

    restore fish's old ^C behavior (cancel-commandline)

--- .config/fish/conf.d/rocka.fish
+++ .config/fish/conf.d/rocka.fish
@@ -2,12 +2,16 @@
 
 # bobthefish theme configuration
 set -g theme_date_format "+%F %a %T"
-set -g theme_date_timezone Asia/Shanghai
+#set -g theme_date_timezone Asia/Shanghai
+
+# restore the old ^C behavior
+bind ctrl-c cancel-commandline
 
 alias ls="ls --color --classify --time-style=long-iso"
 alias l="ls"
 alias ll="ls -lh"
 alias la="ls -alh"
+alias ip="ip --color=auto"
 alias pq="proxychains -q"
 alias qwq="uname -snrm; uptime"
 alias cman="env LANG=zh_CN.UTF-8 man"
